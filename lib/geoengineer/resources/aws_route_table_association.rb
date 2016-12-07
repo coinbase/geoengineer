26495feca66d8873b2229a8efd1f73df3e67078e
Brad Larson <brad@coinbase.com>
2016-12-07 17:39:43 +0000

    | -> ::

--- lib/geoengineer/resources/aws_route_table_association.rb
+++ lib/geoengineer/resources/aws_route_table_association.rb
@@ -7,7 +7,7 @@ class GeoEngineer::Resources::AwsRouteTableAssociation < GeoEngineer::Resource
   validate -> { validate_required_attributes([:subnet_id, :route_table_id]) }
 
   after :initialize, -> { _terraform_id -> { NullObject.maybe(remote_resource)._terraform_id } }
-  after :initialize, -> { _geo_id -> { "#{subnet_id}|#{route_table_id}" } }
+  after :initialize, -> { _geo_id -> { "#{subnet_id}::#{route_table_id}" } }
 
   def self._fetch_remote_resources
     AwsClients
