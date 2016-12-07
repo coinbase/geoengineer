febd6f004fe0ee893a600e86afe892edc2fac3ea
Brad Larson <brad@coinbase.com>
2016-12-07 23:25:16 +0000

    Fix dhcp_otions_spec

--- spec/resources/aws_vpc_dhcp_options_spec.rb
+++ spec/resources/aws_vpc_dhcp_options_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../spec_helper'
 
-describe("GeoEngineer::Resources::AwsVpcDhcpOption") do
-  common_resource_tests(GeoEngineer::Resources::AwsVpcDhcpOption, 'aws_vpc_dhcp_option')
-  name_tag_geo_id_tests(GeoEngineer::Resources::AwsVpcDhcpOption)
+describe("GeoEngineer::Resources::AwsVpcDhcpOptions") do
+  common_resource_tests(GeoEngineer::Resources::AwsVpcDhcpOptions, 'aws_vpc_dhcp_options')
+  name_tag_geo_id_tests(GeoEngineer::Resources::AwsVpcDhcpOptions)
 
   describe "#_fetch_remote_resources" do
     it 'should create list of hashes from returned AWS SDK' do
@@ -10,14 +10,14 @@ describe("GeoEngineer::Resources::AwsVpcDhcpOption") do
       stub = ec2.stub_data(
         :describe_dhcp_options,
         {
-          vpc_dhcp_options: [
-            { dhcp_option_id: 'name1', tags: [{ key: 'Name', value: 'one' }] },
-            { dhcp_option_id: 'name2', tags: [{ key: 'Name', value: 'two' }] }
+          dhcp_options: [
+            { dhcp_options_id: 'name1', tags: [{ key: 'Name', value: 'one' }] },
+            { dhcp_options_id: 'name2', tags: [{ key: 'Name', value: 'two' }] }
           ]
         }
       )
-      ec2.stub_responses(:describe_vpc_dhcp_options, stub)
-      remote_resources = GeoEngineer::Resources::AwsVpcDhcpOption._fetch_remote_resources
+      ec2.stub_responses(:describe_dhcp_options, stub)
+      remote_resources = GeoEngineer::Resources::AwsVpcDhcpOptions._fetch_remote_resources
       expect(remote_resources.length).to eq(2)
     end
   end
