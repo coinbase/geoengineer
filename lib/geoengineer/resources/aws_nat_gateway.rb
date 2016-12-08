23d6f81c12c3e061d1bba612b22bf74bf1c0d42e
Brad Larson <brad@coinbase.com>
2016-12-08 10:11:45 +0000

    New: Add AWS Nat Gateway
    Type of change:
    ===============
    - New feature
    
    What changed? ... and Why:
    ==========================
    Add the ability to codify AWS Nat Gateways.
    
    The only weird thing with this resource is that AWS SDK has buried the
    information about the IP allocation in a `nat_gateway_addresses` array,
    even though a Gateway can have exactly 1 EIP allocation. ¯\_(ツ)_/¯
    
    How has it been tested:
    =======================
    The usual specs.
    
    @mentions:
    ==========
    @grahamjenson
    @lukedemi

---
+++ lib/geoengineer/resources/aws_nat_gateway.rb
@@ -0,0 +1,25 @@
+########################################################################
+# AwsNatGateway is the +aws_nat_gateway+ terrform resource,
+#
+# {https://www.terraform.io/docs/providers/aws/r/nat_gateway.html Terraform Docs}
+########################################################################
+class GeoEngineer::Resources::AwsNatGateway < GeoEngineer::Resource
+  validate -> { validate_required_attributes([:subnet_id, :allocation_id]) }
+
+  after :initialize, -> { _terraform_id -> { NullObject.maybe(remote_resource)._terraform_id } }
+  after :initialize, -> { _geo_id -> { "#{allocation_id}::#{subnet_id}" } }
+
+  def self._fetch_remote_resources
+    AwsClients.ec2.describe_nat_gateways['nat_gateways'].map(&:to_h).map do |gateway|
+      # AWS SDK has `nat_gateway_addresses` as an array, but you should only be able to
+      # have exactly 1 elastic IP association. This logic should cover the bases...
+      allocation = gateway[:nat_gateway_addresses].find { |addr| addr.key?(:allocation_id) }
+      gateway.merge(
+        {
+          _terraform_id: gateway[:nat_gateway_id],
+          _geo_id: "#{allocation[:allocation_id]}::#{gateway[:subnet_id]}"
+        }
+      )
+    end
+  end
+end
