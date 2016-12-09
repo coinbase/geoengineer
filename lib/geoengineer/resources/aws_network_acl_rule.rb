5d0e63faab9a7e7be1dfd2ba38bcb5ddc7649e64
Brad Larson <brad@coinbase.com>
2016-12-09 13:29:40 +0000

    New: Add AWS Network ACL Rule
    Type of change:
    ===============
    - New feature
    
    What changed? ... and Why:
    ==========================
    Adding the ability to codify single network ACL rules.
    
    How has it been tested:
    =======================
    The usual, plus an extra test to make sure the ID was being generated
    correctly. Turns out that was important, because my first implementation
    was all sorts of wrong.

---
+++ lib/geoengineer/resources/aws_network_acl_rule.rb
@@ -0,0 +1,46 @@
+########################################################################
+# AwsNetworkAclRule is the +aws_network_acl_rule+ terrform resource,
+#
+# {https://www.terraform.io/docs/providers/aws/r/network_acl_rule.html Terraform Docs}
+########################################################################
+class GeoEngineer::Resources::AwsNetworkAclRule < GeoEngineer::Resource
+  validate -> {
+    validate_required_attributes(
+      [:network_acl_id, :rule_number, :protocol, :rule_action, :cidr_block]
+    )
+  }
+
+  after :initialize, -> {
+    _terraform_id -> {
+      terraform_id_components = [
+        "#{network_acl_id}-",
+        "#{rule_number}-",
+        "#{egress}-",
+        "#{protocol == 'all' ? '-1' : protocol}-"
+      ]
+      "nacl-#{Crc32.hashcode(terraform_id_components.join)}"
+    }
+  }
+
+  def self._fetch_remote_resources
+    AwsClients
+      .ec2
+      .describe_network_acls['network_acls']
+      .map(&:to_h)
+      .select { |network_acl| !network_acl[:entries].empty? }
+      .map { |network_acl| _generate_rules(network_acl) }
+      .flatten
+  end
+
+  def self._generate_rules(network_acl)
+    network_acl[:entries].map do |rule|
+      terraform_id_components = [
+        "#{network_acl[:network_acl_id]}-",
+        "#{rule[:rule_number]}-",
+        "#{rule[:egress]}-",
+        "#{rule[:protocol] == 'all' ? '-1' : rule[:protocol]}-"
+      ]
+      rule.merge({ _terraform_id: "nacl-#{Crc32.hashcode(terraform_id_components.join)}" })
+    end
+  end
+end
