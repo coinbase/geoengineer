2f2e054c662197fe635fafb99d6912ee1c98b579
Brad Larson <brad@coinbase.com>
2016-12-13 17:27:33 -0800

    Update specs

--- spec/utils/crc32_spec.rb
+++ spec/utils/crc32_spec.rb
@@ -1,10 +1,14 @@
 require_relative '../spec_helper'
 
-describe 'Crc32' do
-  describe "hashcode" do
-    it "should match the output from Terraform's Go Implementation" do
+describe Crc32 do
+  describe "::hashcode" do
+    it "matches the output from Terraform's Go Implementation" do
       expect(Crc32.hashcode("0.0.0.0/0")).to eq(1_080_289_494)
       expect(Crc32.hashcode("10.60.0.0/16")).to eq(2_056_622_336)
+    end
+
+    it "returns 0 for nil strings" do
+      expect(Crc32.hashcode(nil)).to eq(0)
     end
   end
 end
