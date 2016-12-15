51c9216004183e4b0793f97fc6fac914f658e5e1
Brad Larson <brad@coinbase.com>
2016-12-14 16:26:26 -0800

    New: Add aws_main_route_table_association
    Type of change:
    ===============
    - New feature
    
    What changed? ... and Why:
    ==========================
    Somehow this slipped my attention when going through the resources last
    week. Discovered as I was trying to codify our networking layer.
    
    So I added it.
    
    How has it been tested:
    =======================
    Added the usual specs
    
    @mentions:
    ==========
    @grahamjenson

---
+++ lib/geoengineer/resources/aws_main_route_table_association.rb
@@ -0,0 +1,42 @@
+########################################################################
+# AwsMainRouteTableAssociation is the +aws_main_route_table_association+ terrform resource,
+#
+# {https://www.terraform.io/docs/providers/aws/r/main_route_table_assoc.html Terraform Docs}
+########################################################################
+class GeoEngineer::Resources::AwsMainRouteTableAssociation < GeoEngineer::Resource
+  validate -> { validate_required_attributes([:vpc_id, :route_table_id]) }
+
+  after :initialize, -> { _terraform_id -> { NullObject.maybe(remote_resource)._terraform_id } }
+  after :initialize, -> { _geo_id -> { "#{vpc_id}::#{route_table_id}" } }
+
+  def support_tags?
+    false
+  end
+
+  def self._fetch_remote_resources
+    AwsClients
+      .ec2
+      .describe_route_tables['route_tables']
+      .map(&:to_h)
+      .select { |route_table| route_table[:associations] }
+      .map { |route_table| _extract_associations(route_table) }
+      .flatten
+      .select { |association| association[:main] }
+      .map { |association| _merge_ids(association) }
+  end
+
+  def self._merge_ids(association)
+    association.merge(
+      {
+        _terraform_id: association[:route_table_association_id],
+        _geo_id: "#{association[:vpc_id]}::#{association[:route_table_id]}"
+      }
+    )
+  end
+
+  def self._extract_associations(route_table)
+    route_table[:associations].map do |association|
+      association.merge({ vpc_id: route_table[:vpc_id] })
+    end
+  end
+end
