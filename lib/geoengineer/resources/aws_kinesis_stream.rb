78e0ea0c312634bd1ae9b44f1c8e93e84c332ce4
Luke Demi <lddemi@gmail.com>
2017-01-11 14:13:16 -0500

    add remote resource fetching to aws_kinesis_streams

--- lib/geoengineer/resources/aws_kinesis_stream.rb
+++ lib/geoengineer/resources/aws_kinesis_stream.rb
@@ -7,11 +7,8 @@ class GeoEngineer::Resources::AwsKinesisStream < GeoEngineer::Resource
   validate -> { validate_required_attributes([:name, :shard_count]) }
   validate -> { validate_has_tag(:Name) }
 
-  after :initialize, -> {
-    _terraform_id -> {
-      "arn:aws:kinesis:#{environment.region}:#{environment.account_id}:stream/#{self.name}"
-    }
-  }
+  after :initialize, -> { _terraform_id -> { NullObject.maybe(remote_resource)._terraform_id } }
+  after :initialize, -> { _geo_id       -> { self[:name] } }
 
   def to_terraform_state
     tfstate = super
@@ -20,5 +17,24 @@ class GeoEngineer::Resources::AwsKinesisStream < GeoEngineer::Resource
       'shard_count' => self.shard_count.to_s
     }
     tfstate
+  end
+
+  def self._all_streams
+    streams = []
+    AwsClients.kinesis.list_streams[:stream_names].each do |stream_name|
+      AwsClients.kinesis.describe_stream({ stream_name: stream_name }).map(&:to_h).map do |stream|
+        streams << stream[:stream_description]
+      end
+    end
+    streams
+  end
+
+  def self._fetch_remote_resources
+    self._all_streams.map do |stream|
+      stream.merge({
+                     _terraform_id: stream[:stream_arn],
+                     _geo_id: stream[:stream_name]
+                   })
+    end
   end
 end
---
+++ spec/resources/aws_kinesis_stream_spec.rb
@@ -0,0 +1,52 @@
+require_relative '../spec_helper'
+
+describe("GeoEngineer::Resources::AwsKinesisStream") do
+  let(:aws_client) { AwsClients.kinesis }
+
+  before do
+    aws_client.setup_stubbing
+    aws_client.stub_responses(
+      :list_streams,
+      {
+        stream_names: [],
+        has_more_streams: false
+      }
+    )
+  end
+
+  common_resource_tests(GeoEngineer::Resources::AwsKinesisStream, 'aws_kinesis_stream')
+
+  describe "#_fetch_remote_resources" do
+    before do
+      aws_client.stub_responses(
+        :list_streams,
+        {
+          stream_names: ["test"],
+          has_more_streams: false
+        }
+      )
+    end
+    before do
+      aws_client.stub_responses(
+        :describe_stream,
+        {
+          stream_description:
+          {
+            stream_name: "test",
+            stream_arn: "arn:aws:kinesis:us-east-1:1234567890:stream/test",
+            stream_status: "ACTIVE",
+            shards: [],
+            has_more_shards: false,
+            retention_period_hours: 24,
+            enhanced_monitoring: []
+          }
+        }
+      )
+    end
+
+    it 'should create list of hashes from returned AWS SDK' do
+      remote_resources = GeoEngineer::Resources::AwsKinesisStream._fetch_remote_resources
+      expect(remote_resources.length).to eq 1
+    end
+  end
+end
