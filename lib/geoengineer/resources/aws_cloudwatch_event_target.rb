2f3c4a1164f7f06677201a8bbe272930c1253c73
Luke Demi <lddemi@gmail.com>
2017-01-12 15:56:53 -0500

    New: add cloudwatch_event_target

---
+++ lib/geoengineer/resources/aws_cloudwatch_event_target.rb
@@ -0,0 +1,50 @@
+########################################################################
+# AwsCloudwatchEventTarget is the +aws_cloudwatch_event_target+ terrform resource,
+#
+# {https://www.terraform.io/docs/providers/aws/r/cloudwatch_event_target.html Terraform Docs}
+########################################################################
+class GeoEngineer::Resources::AwsCloudwatchEventTarget < GeoEngineer::Resource
+  validate -> { validate_required_attributes([:rule, :arn, :target_id]) }
+
+  after :initialize, -> { _terraform_id -> { NullObject.maybe(remote_resource)._terraform_id } }
+  after :initialize, -> { _geo_id       -> { "#{self.rule}-#{self.target_id}" } }
+
+  def to_terraform_state
+    tfstate = super
+    tfstate[:primary][:attributes] = {
+      'target_id' => self.target_id,
+      'rule' => self.rule
+    }
+    tfstate
+  end
+
+  def support_tags?
+    false
+  end
+
+  def self._fetch_remote_resources
+    AwsClients
+      .cloudwatchevents
+      .list_rules
+      .rules
+      .map(&:to_h)
+      .map { |rule| _get_rule_targets(rule) }
+      .flatten
+      .map do |rule_target|
+      rule_target.merge(
+        {
+          _terraform_id: "#{rule_target[:rule_name]}-#{rule_target[:id]}",
+          _geo_id: "#{rule_target[:rule_name]}-#{rule_target[:id]}"
+        }
+      )
+    end
+  end
+
+  def self._get_rule_targets(rule)
+    AwsClients
+      .cloudwatchevents
+      .list_targets_by_rule({ rule: rule[:name] })[:targets]
+      .map(&:to_h)
+      .map { |target| target.merge({ rule_name: rule[:name] }) }
+  end
+end
