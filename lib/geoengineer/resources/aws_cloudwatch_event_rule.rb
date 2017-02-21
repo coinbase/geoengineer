f5a86e2bbd8d90f9f4be54b815239ea316ee8783
Julian Langschaedel <meta.rb@gmail.com>
2017-02-21 17:21:57 +0100

    improve AwsCloudwatchEventRule validation

--- lib/geoengineer/resources/aws_cloudwatch_event_rule.rb
+++ lib/geoengineer/resources/aws_cloudwatch_event_rule.rb
@@ -4,7 +4,14 @@
 # {https://www.terraform.io/docs/providers/aws/r/cloudwatch_event_rule.html Terraform Docs}
 ########################################################################
 class GeoEngineer::Resources::AwsCloudwatchEventRule < GeoEngineer::Resource
-  validate -> { validate_required_attributes([:name, :schedule_expression]) }
+  validate -> { validate_required_attributes([:name]) }
+  validate -> { validate_schedule_or_event }
+
+  def validate_schedule_or_event
+    return if !self[:schedule_expression] && self[:event_pattern]
+    return if self[:schedule_expression] && !self[:event_pattern]
+    ["#{self.id}: Need either schedule_expression or event_pattern defined"]
+  end
 
   after :initialize, -> { _terraform_id -> { NullObject.maybe(remote_resource)._terraform_id } }
   after :initialize, -> { _geo_id       -> { self[:name] } }
