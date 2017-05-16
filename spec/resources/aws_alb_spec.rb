d7e590c0f9df7f3fded7d21be25313c558c7f846
Brad Larson <brad@coinbase.com>
2017-05-16 13:29:11 +0100

    [New] Add AWS ALB to supported resources
    What changed? ... and Why:
    ==========================
    Added a wrapper for ALB's and added some tests as well.
    
    The ELB chose to enforce the name being present, which precludes the use
    of the name prefix. In practice, that was fine, in part because the name
    was then used as the terraform ID. With ALB's, the name is only part of
    the ID, and the rest contains what looks to be some random value.
    
    This means that the best way to keep track of the resource is via the
    tags, however that means that an extra call is required to grab the
    tags.
    
    How has it been tested:
    =======================
    Added the usual resource tests.

---
+++ spec/resources/aws_alb_spec.rb
@@ -0,0 +1,33 @@
+require_relative '../spec_helper'
+
+describe GeoEngineer::Resources::AwsAlb do
+  let(:alb_client) { AwsClients.alb }
+
+  common_resource_tests(described_class, described_class.type_from_class_name)
+
+  before { alb_client.setup_stubbing }
+
+  describe "#_fetch_remote_resources" do
+    it 'should create list of hashes from returned AWS SDK' do
+      alb_client.stub_responses(
+        :describe_load_balancers,
+        {
+          load_balancers: [{ load_balancer_arn: "foo/bar-baz" }]
+        }
+      )
+      alb_client.stub_responses(
+        :describe_tags,
+        {
+          tag_descriptions: [
+            {
+              resource_arn: "foo/bar-baz",
+              tags: [{ key: "Name", value: "foo/bar-baz" }]
+            }
+          ]
+        }
+      )
+      remote_resources = GeoEngineer::Resources::AwsAlb._fetch_remote_resources(nil)
+      expect(remote_resources.length).to eq 1
+    end
+  end
+end
