c0f47f13a5498fe889befa3dc3a5cdf64ce34ca5
Tyler Hampton <dr.frankinfurter@gmail.com>
2017-06-06 13:09:46 -0700

    ISSUE-#125_aws_s3_bucket_object: adding support for Terraform resource aws_s3_bucket_object

---
+++ lib/geoengineer/resources/aws_s3_bucket_object.rb
@@ -0,0 +1,24 @@
+########################################################################
+# AwsS3Bucket is the +aws_s3_bucket_object+ terrform resource,
+#
+# {https://www.terraform.io/docs/providers/aws/r/s3_bucket_object.html Terraform Docs}
+########################################################################
+class GeoEngineer::Resources::AwsS3BucketObject < GeoEngineer::Resource
+  validate -> { validate_required_attributes([:bucket]) }
+
+  after :initialize, -> { _terraform_id -> { bucket } }
+
+  def to_terraform_state
+    tfstate = super
+    tfstate[:primary][:attributes] = {
+      'bucket' => bucket,
+      'key'    => key,
+      'source' => source
+    }
+    tfstate
+  end
+
+  def short_type
+    's3_object'
+  end
+end
