d2376d17f3f4297409945894674ca64303382191
Ken Robertson <ken@invalidlogic.com>
2017-11-07 10:48:45 -0800

    Updates route53_zone to handle differentiating public/private zones.
    This fixes `aws_route53_zone` to include whether the zone is public or private
    one belonging to a VPC in the `geo_id`. This helps in cases where the same
    domain has two or more zones, such as a public one and a private one internal to
    a VPC.

--- spec/resources/aws_route53_zone_spec.rb
+++ spec/resources/aws_route53_zone_spec.rb
@@ -11,7 +11,10 @@ describe(GeoEngineer::Resources::AwsRoute53Zone) do
       aws_client.stub_responses(
         :list_hosted_zones, {
           hosted_zones: [
-            { id: '123',  name: "testzone", caller_reference: "test" },
+            { id: '123',  name: "testzone", caller_reference: "test",
+              config: { private_zone: false } },
+            { id: '124',  name: "testzone", caller_reference: "test",
+              config: { private_zone: true } },
             { id: '1234', name: "anothertestzone", caller_reference: "test" }
           ],
           is_truncated: false,
@@ -19,15 +22,28 @@ describe(GeoEngineer::Resources::AwsRoute53Zone) do
           marker: "foo"
         }
       )
+
+      aws_client.stub_responses(
+        :get_hosted_zone, {
+          hosted_zone: {
+            id: '124', name: "testzone", caller_reference: "test"
+          },
+          vp_cs: [{ vpc_id: "myvpc" }]
+        }
+      )
     end
 
     it 'should create an array of hashes from the AWS response' do
       resources = GeoEngineer::Resources::AwsRoute53Zone._fetch_remote_resources(nil)
-      expect(resources.count).to eql(2)
+      expect(resources.count).to eql(3)
 
       testzone = resources.first
       expect(testzone[:_terraform_id]).to eql('123')
-      expect(testzone[:_geo_id]).to eql('testzone')
+      expect(testzone[:_geo_id]).to eql('public-testzone')
+
+      testzone = resources[1]
+      expect(testzone[:_terraform_id]).to eql('124')
+      expect(testzone[:_geo_id]).to eql('myvpc-testzone')
     end
   end
 end
