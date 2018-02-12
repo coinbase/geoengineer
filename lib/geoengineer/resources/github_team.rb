ea1eb1b3c052af4c017b5dc6fd304c2f44607c62
Shane da Silva <shane@dasilva.io>
2018-02-11 23:25:47 -0800

    Add github_team resource
    Allows you to create/manage teams within an org.

---
+++ lib/geoengineer/resources/github_team.rb
@@ -0,0 +1,19 @@
+################################################################################
+# GithubTeam is the +github_team+ Terraform resource.
+#
+# {https://www.terraform.io/docs/providers/github/r/team.html Terraform Docs}
+################################################################################
+class GeoEngineer::Resources::GithubTeam < GeoEngineer::Resource
+  validate -> { validate_required_attributes([:name]) }
+
+  after :initialize, -> { _terraform_id -> { NullObject.maybe(remote_resource)._terraform_id } }
+  after :initialize, -> { _geo_id -> { name } }
+
+  def self._fetch_remote_resources(provider)
+    GithubClient.organization_teams(provider.organization)
+                 .each do |team|
+      team[:_terraform_id] = team[:id].to_s
+      team[:_geo_id] = team[:name]
+    end
+  end
+end
