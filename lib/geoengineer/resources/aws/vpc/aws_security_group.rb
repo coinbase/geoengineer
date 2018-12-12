885831bbde70245ee687052ebf81e7c0fc569bec
graham jenson <graham.jenson@coinbase.com>
2018-12-12 15:24:07 +1300

    fix order

--- lib/geoengineer/resources/aws/vpc/aws_security_group.rb
+++ lib/geoengineer/resources/aws/vpc/aws_security_group.rb
@@ -20,6 +20,49 @@ class GeoEngineer::Resources::AwsSecurityGroup < GeoEngineer::Resource
   after :initialize, -> { _terraform_id -> { NullObject.maybe(remote_resource)._terraform_id } }
   after :initialize, -> { _geo_id       -> { NullObject.maybe(tags)[:Name] } }
 
+  def to_terraform_state
+    tfstate = super
+
+    egress_ingress = {
+      "egress.#": all_egress.length.to_s,
+      "ingress.#": all_ingress.length.to_s
+    }
+
+    c = 0
+    all_egress.each do |e|
+      c += 1
+      add_egress_ingress_to_state(egress_ingress, "egress.#{c}", e, e.cidr_blocks || [], e.security_groups || [])
+    end
+
+    all_ingress.each do |e|
+      c += 1
+      add_egress_ingress_to_state(egress_ingress, "ingress.#{c}", e, e.cidr_blocks || [], e.security_groups || [])
+    end
+
+    tfstate[:primary][:attributes] = egress_ingress
+    tfstate
+  end
+
+  def add_egress_ingress_to_state(egress_ingress, prefix, e, cb, sg)
+    egress_ingress.merge!({
+                            "#{prefix}.from_port" => e.from_port.to_s,
+                            "#{prefix}.to_port" => e.to_port.to_s,
+                            "#{prefix}.protocol" => e.protocol.to_s,
+                            "#{prefix}.self" => e["self"].to_s,
+                            "#{prefix}.cidr_blocks.#" => cb.length.to_s,
+                            "#{prefix}.security_groups.#" => sg.length.to_s
+                          })
+
+    add_list_to_state(egress_ingress, prefix, cb, "cidr_blocks")
+    add_list_to_state(egress_ingress, prefix, sg, "security_groups")
+  end
+
+  def add_list_to_state(egress_ingress, prefix, list, list_type)
+    list.each_with_index do |r, i|
+      egress_ingress["#{prefix}.#{list_type}.#{i}"] = r.to_s
+    end
+  end
+
   def flatten_cidr_and_sg_blocks
     (self.all_ingress + self.all_egress).each do |in_eg|
       in_eg.cidr_blocks      = in_eg.cidr_blocks.flatten     if in_eg.cidr_blocks
