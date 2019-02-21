8734b73271e518d30438a512cc0c8e6b9cb7363d
Ken Robertson <ken@invalidlogic.com>
2019-02-21 15:22:22 -0800

    Fix class name for resource on hosted Direct Connect interfaces
    This fixes the class name for the
    `aws_dx_hosted_private_virtual_interface_accepter` resource. The ruby
    file was named `accepter` and all, however the class name was previously
    `acceptor`. This was incorrect and didn't catch until looked at it very
    closely. :smile:

--- spec/resources/aws_dx_hosted_private_virtual_interface_accepter_spec.rb
+++ spec/resources/aws_dx_hosted_private_virtual_interface_accepter_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../spec_helper'
 
-describe(GeoEngineer::Resources::AwsDxHostedPrivateVirtualInterfaceAcceptor) do
+describe(GeoEngineer::Resources::AwsDxHostedPrivateVirtualInterfaceAccepter) do
   let(:aws_client) { AwsClients.directconnect }
 
   common_resource_tests(described_class, described_class.type_from_class_name)
@@ -23,7 +23,7 @@ describe(GeoEngineer::Resources::AwsDxHostedPrivateVirtualInterfaceAcceptor) do
     end
 
     it 'should create list of hashes from returned AWS SDK' do
-      remote_resources = GeoEngineer::Resources::AwsDxHostedPrivateVirtualInterfaceAcceptor._fetch_remote_resources(nil)
+      remote_resources = GeoEngineer::Resources::AwsDxHostedPrivateVirtualInterfaceAccepter._fetch_remote_resources(nil)
       expect(remote_resources.length).to eq(2)
     end
   end
