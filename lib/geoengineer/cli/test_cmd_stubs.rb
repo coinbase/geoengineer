3641ed421210679532cfa453cc5f6b127439436f
graham jenson <graham.jenson@coinbase.com>
2019-04-15 11:03:36 +0100

    ruboco

--- lib/geoengineer/cli/test_cmd_stubs.rb
+++ lib/geoengineer/cli/test_cmd_stubs.rb
@@ -4,7 +4,12 @@ module GeoCLI::TestCmdStubs
     puts "Stubbing Commands"
     AwsClients.stub!
 
-    # This method errors for some reason
+    # These are resource calls that aws has issues auto-creating stubs for
+    kinesis_bad_stubs
+    cloudfront_bad_stubs
+  end
+
+  def self.kinesis_bad_stubs
     AwsClients.kinesis.stub_responses(
       :list_streams,
       {
@@ -12,30 +17,21 @@ module GeoCLI::TestCmdStubs
         has_more_streams: false
       }
     )
+  end
 
+  def self.cloudfront_bad_stubs
     AwsClients.cloudfront.stub_responses(
-      :list_cloud_front_origin_access_identities,
-      {
+      :list_cloud_front_origin_access_identities, {
         cloud_front_origin_access_identity_list: {
-          items: [],
-          marker: "marker",
-          max_items: 100,
-          is_truncated: false,
-          quantity: 0
+          items: [], marker: "marker", max_items: 100, is_truncated: false, quantity: 0
         }
       }
     )
 
     AwsClients.cloudfront.stub_responses(
-      :list_distributions,
-      {
-        distribution_list:
-        {
-          items: [],
-          marker: "marker",
-          max_items: 100,
-          is_truncated: false,
-          quantity: 0
+      :list_distributions, {
+        distribution_list: {
+          items: [], marker: "marker", max_items: 100, is_truncated: false, quantity: 0
         }
       }
     )
